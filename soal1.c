06c1d37746922c087b9bcb1895481c0146299d1a
Audrin Frederick <audrinnaufal@gmail.com>
2026-04-13 09:18:00 +0700

    2

--- soal1.c
+++ soal1.c
@@ -11,34 +11,39 @@
  #include <stdio.h>
  #include <string.h>
   
- int main() {
-  int izin, i;
+ int main(void) {
+  int izin;
   float suhu;
   unsigned int radiasi;
   unsigned int jam;
 
-  printf("Masukkan izin suhu radiasi jam: ");
+  printf("Masukkan izin, suhu, radiasi, dan jam (misal: 1 36.5 2 10): ");
   scanf("%d %f %u %u", &izin, &suhu, &radiasi, &jam);
 
-    if (radiasi >=6) {
+
+    int jam_ditolak = (jam < 6 || jam > 20);   // jam di luar 6-20 ditolak untuk izin 1
+    int jam_diterima = (jam >= 6 && jam <= 18); // jam 6-18 diterima untuk izin >=2
+
+    if (radiasi >= 6) {
         printf("TOLAK");
     } else if (suhu >= 390) {
         printf("KARANTINA");
-    } else if (izin ==1 && (jam<6 || jam>20)) {
+    } else if (izin == 1 && jam_ditolak) {
+        // Izin 1 hanya boleh masuk antara jam 6 sampai 20 (inklusif)
         printf("TOLAK");
-    } else if (izin ==1) {
+    } else if (izin == 1) {
         printf("MASUK");
-    } else if (izin >= 2 && radiasi <= 2 && (jam >= 6 && jam <= 18)) {
+    } else if (izin >= 2 && radiasi <= 2 && jam_diterima) {
+        // Izin >=2, radiasi rendah, dan jam 6-18 boleh masuk
         printf("MASUK"); 
-    } else if (izin == 2 ) {
-        printf("PEMERIKSAAN");
-    } else if (izin == 3 && radiasi == 0 && suhu < 380) {
-        printf("MASUK");
-    } else {
-        printf("TOLAK");
+        } else if (izin == 2 ) {
+            printf("PEMERIKSAAN");
+        } else {
+            printf("TOLAK");
+        }
+        return 0;
     }
-     return 0;
-    }
+
 
 /** RE FERENSI
  * https://www.geeksforgeeks.org/c/data-types-in-c/
